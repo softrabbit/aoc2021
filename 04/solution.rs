c4e13f9940b6e188e266edd9ccf29978b01d5d01
Raine M. Ekman <raine@iki.fi>
2021-12-06 19:55:29 +0200

    Day 4, part 2. Had to redo the logic for marking used numbers, as there was a zero bug in the first version. Now using tuples instead of negative values.

--- 04/solution.rs
+++ 04/solution.rs
@@ -1,42 +1,44 @@
 // https://adventofcode.com/2021/day/4
 
-// First line, bingo numbers. After that, bingo cards.
+// First line, bingo numbers. After that, bingo board.
 // Find winning card and sum unmarked numbers.
+
+// Part 1: find card that is first to win
+// Part 2: find card that is last to win
 
 use std::io::{self, BufRead};
 
 // Check for bingo in 5x5 board
-fn check_bingo(board: [i32; 25]) -> bool {
+fn check_bingo(board: [(i32, bool); 25]) -> bool {
     let mut bingo = false;
     // Horizontal
     for offset in (0..25).step_by(5) {
         bingo = bingo || ( board[offset..offset+5].into_iter()
-                   .filter(|i| i < &&0)
-                   .collect::<Vec<&i32>>()
+                   .filter(|(_i, b)| *b)
+                   .collect::<Vec<&(i32,bool)>>()
                    .len() == 5);
     }
     // Vertical
     for offset in 0..5 {
         let mut tmp = true;
-        for row in (0..25).step_by(5) {
-            tmp = tmp && (board[offset + row] < 0);
+        for location in (offset..offset+25).step_by(5) {
+            tmp = tmp && (board[location].1);
         }
         bingo = bingo || tmp;
     }
-
-
     return bingo;
 }
 
-// Using negative values for marked numbers
-fn mark_number(n: i32, board: &mut [i32; 25]) {
-    *board = board.map(|i| if i==n { i*-1 } else { i });
+// Mark any number n with true
+fn mark_number(n: i32, board: &mut [(i32, bool); 25]) {
+    *board = board.map(|(i,b)| (i, if i==n {true} else {b} ) );
 }
 
-fn sum_unmarked(board: [i32; 25]) -> i32 {
+fn sum_unmarked(board: [(i32, bool); 25]) -> i32 {
     //let sum: i32 = board.iter().sum();
     return  board.iter()
-        .filter(|i| *i > &0)
+        .map(|(i,b)| if *b {0} else {*i})
+        //.filter(|i| *i > &0)
         // .collect::<Vec<&i32>>()
         .sum();
 }
@@ -57,12 +59,12 @@ fn main() {
     // Build list of boards    
     // 2D arrays are cool, but maybe this will do...
 
-    let mut boards = Vec::<[i32; 25]>::new();    
+    let mut boards = Vec::<[(i32, bool); 25]>::new();    
 
     while iterator.peek().is_some() {
         // Consume blank line
         let _dummy = iterator.next();
-        let mut board: [i32; 25] =  [0; 25];
+        let mut board: [(i32, bool); 25] =  [(0, false); 25];
         for offset in (0..25).step_by(5) {
             let row: Vec<i32> = iterator.next().unwrap().unwrap()
                 .split(' ').map(|s| s.trim() )
@@ -70,31 +72,29 @@ fn main() {
                 .map(|s| s.parse::<i32>().unwrap() )
                 .collect();
             for (idx, n) in row.iter().enumerate() {
-                board[offset + idx] = *n;
+                board[offset + idx] = (*n, false);
             }
         }
         boards.push(board);
     }
     
     // Iterate through list of numbers, mark and check boards...
-    //let mut n: i32;
-    let mut win = -1;
 
-    'outer_loop:
+    let mut win;
+    let mut skip_boards = Vec::new();
+        
     for n in &numbers {
         // How obvious: this doesn't work, but an explicit index does...
         // for (idx, b) in boards.iter().enumerate() {
         for idx in 0..boards.len() {
             mark_number(*n, &mut boards[idx]);
             
-            if check_bingo(boards[idx]) {
+            if !skip_boards.contains(&idx) && check_bingo(boards[idx]) {
+                skip_boards.push(idx);
                 win = n * sum_unmarked(boards[idx]);
-                break 'outer_loop; // a GOTO by any other name...
+                // Print out all wins, which will satisfy both part 1 and 2.
+                println!("Win, score {:?} in board {:?} after {:?}",win, idx,n);
             }
         }
     }
-    
-    println!("{:?}", boards);
-    
-    println!("The number is {:?}", win);
 }
