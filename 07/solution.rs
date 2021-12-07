60f58aef6af29f08ca05cfe63afc46ce91730685
Raine M. Ekman <raine@iki.fi>
2021-12-07 10:16:28 +0200

    Day 7 solved, not in an optimal way but correct.

---
+++ 07/solution.rs
@@ -0,0 +1,54 @@
+
+// https://adventofcode.com/2021/day/7
+
+// Reads from stdin and prints a number.
+// rustc --cfg part2 for the second case.
+
+use std::io::{self, BufRead};
+
+// In part 2, costs are calculated in an exponential way.
+// Basically, sum all numbers up to n. Recursion is stupid, too.
+fn costof(n: i64) -> i64 {
+    return if n == 0 { 0 } else { n + costof(n -1) };
+}
+
+
+fn main() {
+    let stdin = io::stdin();
+    let mut iterator = stdin.lock().lines();
+
+    // First line holds the positions
+    let line1 = iterator.next().unwrap().unwrap();
+    let numbers: Vec<i64> = line1.split(',')
+        .map(|s| s.parse::<i64>().unwrap() )
+        .collect();
+
+    // Align the elements to the same X.
+    // Every step costs 1 fuel.
+    // I think it's pretty obvious that the 
+    // potential spots for aligning
+    // should be between max and min of the array?
+    let mut min = numbers[0];
+    let mut max = numbers[0];
+    for n in &numbers {
+        min = if n < &min {*n} else {min};
+        max = if n > &max {*n} else {max};
+    }
+
+    // Brute forcing it
+    let mut min_cost = i64::MAX;
+    let mut min_idx = 0;
+    for n in min..=max {
+        #[cfg(not(part2))]
+        let cost = numbers.iter().map(|i| (n-i).abs()).sum();
+        #[cfg(part2)]
+        let cost = numbers.iter().map(|i| costof((n-i).abs()) ).sum();
+        if cost < min_cost {
+            min_cost = cost;
+            min_idx = n;
+        }
+    }
+
+
+    println!("Minimum cost {:?} at {:?}", min_cost, min_idx);
+}
