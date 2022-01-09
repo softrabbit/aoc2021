65ee7cd13e60976199b21a0bb52689c41b7ebd91
Raine M. Ekman <raine@iki.fi>
2022-01-09 14:37:54 +0200

    Returned to day 3. Seems like my memory is a bit rusty (not in a good way) after a month since last touching this.

--- 03/solution2.rs
+++ 03/solution2.rs
@@ -4,74 +4,71 @@
 //         filters and such... and the logic is flawed too, for now.
 
 use std::io::{self, BufRead};
-// use std::option::Option;
 
-// Filters on the binstr matching value in position index...
-fn myFilter(index: usize, value: char, binstr: &str) -> bool {
-    if binstr.chars().nth(index).unwrap() == value {
-        return true;
-    } else {
-        return false;
-    }
+// Count bit in position 'index' in supplied binary strings
+fn count_bits(index: usize, binstrs: &Vec<String>) -> i32 {
+    binstrs
+        .iter()
+        .map(|s| if s
+             .chars()
+             .nth(index)
+             .unwrap() == '1' {
+                 1
+             } else {
+                 -1
+             })
+        .sum()
 }
 
 fn main() {
     let stdin = io::stdin();
-    let mut counters = Vec::new();
     let mut data = Vec::new();
-    let mut binary;
     for line in stdin.lock().lines() {
         let lstr = line.unwrap(); // Can only unwrap once because ownership
         data.push(lstr.clone());
-        binary = lstr.chars();
-        while lstr.len() > counters.len() {
-            counters.push(0);
-        }
-        for (index, c) in binary.enumerate() {
-            if c == '1' {
-                counters[index] += 1;
-            } else {
-                counters[index] -= 1;
-            }
-        }
     }
+    
     // Here the counters are >0 for more ones than zeros and vice versa
+    // Equal number selects 1
     let mut data_tmp = data.clone();
-    for (index, c) in counters.iter().enumerate() {
+
+    for i in 0..data[0].len() {
         if data_tmp.len() > 1 {
-            let val = if *c > 0 { '1' } else { '0'};
+            let c = count_bits(i, &data_tmp);
+
+            let val = if c >= 0 { '1' } else { '0'};
             // println!("{}",c);
             data_tmp = data_tmp
                 .into_iter() // NB iter() doesn't work here...
-                .filter(|item| item.chars().nth(index).unwrap() == val )
-                .collect();            
-        }        
+                .filter(|item| item.chars().nth(i).unwrap() == val )
+                .collect();
+            // println!("{:?} {:?} {:?}",i,c,data_tmp);
+        }
     }
+
     // We hope there is only one string left...
     let oxygen = isize::from_str_radix(&data_tmp[0], 2).unwrap();
 
-    // And then the same stuff for the _least_ seen digits
+    
+    // And then the same stuff for the _least_ seen digits, so the logic is reversed
+    // These could really be in one loop
     data_tmp = data.clone();
-    for (index, c) in counters.iter().enumerate() {        
-        print!("{} ",c);
+
+    for i in 0..data[0].len() {
         if data_tmp.len() > 1 {
-            let val = if *c < 0 { '1' } else { '0'};
+            let c = count_bits(i, &data_tmp);
+
+            let val = if c >= 0 { '0' } else { '1'};
+            // println!("{}",c);
             data_tmp = data_tmp
                 .into_iter() // NB iter() doesn't work here...
-                .filter(|item| item.chars().nth(index).unwrap() == val )
-                .collect();            
-        }        
+                .filter(|item| item.chars().nth(i).unwrap() == val )
+                .collect();
+        }
     }
-    // We hope there is only one string left...
+
     let co2 = isize::from_str_radix(&data_tmp[0], 2).unwrap();
     println!("");
-    
-    // for line in data_tmp {
-    //     println!("{}",line);
-    // }
-    // println!("{}",data_iter.count());
-
-    
 
     println!("The number is {:?} ({} * {})", oxygen * co2, oxygen, co2);
 }
